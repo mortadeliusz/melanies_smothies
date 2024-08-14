3ec8f2ea5b56c861b71fcbeceed456cbc1e4e70d
Maciej Ryzejno <mryzejno@gmail.com>
2024-08-14 16:25:42 +0200

    utilizing dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 ingredients_list = st.multiselect("select up to 5 ingredients",my_dataframe,max_selections=5)
 # temp part
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True )
 # end of temp part
 if ingredients_list:
     ingredients_string=''
