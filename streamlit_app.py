773cb4f8dad64af49a2d9297f6e9002c14c83bc5
Maciej Ryzejno <mryzejno@gmail.com>
2024-08-16 15:29:23 +0200

    adding simple write statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -14,6 +15,9 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect("select up to 5 ingredients",my_dataframe,max_selections=5)
 # temp part
@@ -23,6 +27,10 @@ if ingredients_list:
     ingredients_string=''
     for fruit in ingredients_list:
         ingredients_string+=fruit+' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(f'{fruit} Nutrion Information.')
         fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit}')
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True )
